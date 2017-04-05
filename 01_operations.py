03a475c5b4359b477b226b2210b1bf3898d683bb
Dries <dries.cronje@outlook.com>
2017-04-05 15:54:40 +0200

    Added more operations

--- 01_operations.py
+++ 01_operations.py
@@ -17,12 +17,22 @@ A = tf.convert_to_tensor(np.array([[1., 2.], [4., 5.]]))
 # 2x2 Matrix
 B = tf.convert_to_tensor(np.array([[-1., -3.], [2., -1.]]))
 
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+
 add = A + B
 mul = tf.matmul(A, B)
+mul2 = tf.matmul(a,b)
 
 print('Add:')
 print(sess.run(add))
 
-print('\r\nMultiply:')
+print('\r\nMultiply 1:')
 print(sess.run(mul))
+
+print('\r\nMultiply 2')
+print(sess.run(mul2, {a: [[1,2,3],[4,5,6]], b: [[7,8],[9,10],[11,12]]}))
+
+print('\r\nMultiply 3')
+print(sess.run(mul2, {a: [[1,2,3],[4,5,6]], b: B}))
 
